14d0530a0d4826297238d774f706a742c4a79eea
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-05 13:14:00 -0300

    solucion click en boton

--- eventos.js
+++ eventos.js
@@ -1,5 +1,18 @@
 const div = document.querySelector('div');
+const btn = document.querySelector('button');
 
 div.addEventListener('click', () => {
     alert('Hola! Soy el div');
 });
+
+btn.addEventListener('click', (e) => {
+    alert('Hola!');
+    e.stopPropagation();
+    div.removeEventListener('click', saludoDIv);
+});
+
+function saludoDIv() {
+    alert('Hola! Soy el div');
+}
+
+div.addEventListener('click', saludoDIv);
